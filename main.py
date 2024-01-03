ec10e276bcbc66abcb37441eb305de5cd5a285c9
Jonathan Cann <jonathancann101@gmail.com>
2024-01-03 20:55:59 +0000

    dir_dict added.

--- main.py
+++ main.py
@@ -31,6 +31,20 @@ class Photo:
             new_image = original_image.resize(new_size, Image.ANTIALIAS)
             new_image.save(os.path.join(self.new_folder, f"scaled_{new_size[0]}x{new_size[1]}.jpg"), quality=100, dpi=(300,300))
 
+# Directory creation list.
+dir_dict = {'4x5in.jpg': '4x5 Aspect Ratio',
+            '4x6in.jpg': '2x3 Aspect Ratio',
+            '5x7in.jpg': '5x7 Aspect Ratio',
+            '6x8in.jpg': '3x4 Aspect Ratio',
+            '11x14in.jpg': '11x14 Aspect Ratio',
+            'A5P.jpg': 'ISO Sizes',
+            '5x4in.jpg': '5x4 Aspect Ratio',
+            '6x4in.jpg': '3x2 Aspect Ratio',
+            '7x5in.jpg': '7x5 Aspect Ratio',
+            '8x6in.jpg': '4x3 Aspect Ratio',
+            '14x11in.jpg': '14x11 Aspect Ratio',
+            'A5L.jpg': 'ISO Sizes'}
+
 size_dict = {"large": (1200, 800), "medium": (600, 400)}
 scaler = PhotoScaler("path/to/root/folder", size_dict)
 scaler.scale_photos()
